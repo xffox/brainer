91ecfc2efc95c1f88525ab144ad76ac823ef4c1a
xffox <0xff0x666@gmail.com>
2016-04-04 09:07:52 +0900

    bot configurability

--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -3,12 +3,64 @@
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <locale>
+#include <codecvt>
 
-#include "csv/csv.h"
+#include "base/FileConfig.h"
+
+namespace
+{
+    struct Config
+    {
+        std::string jid;
+        std::string password;
+    };
+
+    Config readConfig(const std::string &filename)
+    {
+        using ConfMap = std::unordered_map<std::wstring, std::wstring>;
+        base::FileConfig fc(filename);
+        base::IConfig::ValuesCollection values;
+        try
+        {
+            values = fc.read();
+        }
+        catch(const std::exception &exc)
+        {
+            throw std::runtime_error("failed to read config");
+        }
+        ConfMap conf(values.begin(), values.end());
+        std::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
+        Config result;
+        {
+            auto jidIter = conf.find(L"jid");
+            if(jidIter == conf.end())
+                throw std::runtime_error("config: jid missing");
+            result.jid = convert.to_bytes(jidIter->second);
+        }
+        {
+            auto passwordIter = conf.find(L"password");
+            if(passwordIter == conf.end())
+                throw std::runtime_error("config: password missing");
+            result.password = convert.to_bytes(passwordIter->second);
+        }
+        return result;
+    }
+}
 
 int main()
 {
-    bot::Bot bot("", "");
-    bot.run();
+    try
+    {
+        const auto config = readConfig("brainer_bot.conf");
+        bot::Bot bot(config.jid, config.password);
+        bot.run();
+    }
+    catch(const std::exception &exc)
+    {
+        std::cerr<<"error: "<<exc.what()<<std::endl;
+    }
     return 0;
 }
